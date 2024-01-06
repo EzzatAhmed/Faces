4ab17c1803ded325c1dfa2e041c208c9026b8272
Ezzat Ahmed <38844471+EzzatAhmed@users.noreply.github.com>
2024-01-06 21:56:07 +0200

    Add project files.

---
+++ Messaging.InterfacesConstants/Commands/IRegisterOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.InterfacesConstants.Commands
+{
+     public interface IRegisterOrderCommand
+    {
+         string PictureUrl { get; set; }
+         string UserEmail { get; set; }
+         byte[] ImageData { get; set; }
+    }
+}
